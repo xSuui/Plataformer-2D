4f7d4c3c02cfe8d8229ebcbbe4a2147adbc275d2
xSuui <samuel2012rogerio@gmail.com>
2023-09-29 16:39:54 -0300

    add camera follows the player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,23 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
+    [Header("Speed setup")]
     public Vector2 friction = new Vector2(.1f, 0);
-
     public float speed;
     public float speedRun;
-
-
     public float forceJump = 2;
 
+    [Header("Animation setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = 0.7f;
+    public float animationDuration = .3f;
+    public Ease ease = Ease.OutBack;
+
+
+    private void Start()
+    {
+        DOTween.SetTweensCapacity(200, 125);
+    }
+
+    //public float landingScaleY = 1.5f;
+    //public float landingScaleX = 1.5f;
+   
     private float _currentSpeed;
-
-
     //private bool _isRunning = false;
+
 
     private void Update()
     {
@@ -60,7 +73,28 @@ public class Player : MonoBehaviour
     private void HandleJump()
     {
         if (Input.GetKey(KeyCode.Space))
+        {
             myRigidbody.velocity = Vector2.up * forceJump;
+            myRigidbody.transform.localScale = Vector2.one;
+
+            DOTween.Kill(myRigidbody.transform);
+
+            HandleScaleJump();
+
+            //handleScaleLanding();
+        }    
     }
 
+
+    private void HandleScaleJump()
+    {
+        myRigidbody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigidbody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+    }
+
+    /*private void handleScaleLanding()
+    {
+        //myRigidbody.transform.DOScaleY(landingScaleY, animationDuration).SetLoops(2, LoopType.Yoyo);
+        //myRigidbody.transform.DOScaleX(landingScaleX, animationDuration).SetLoops(2, LoopType.Yoyo);
+    }*/
 }
