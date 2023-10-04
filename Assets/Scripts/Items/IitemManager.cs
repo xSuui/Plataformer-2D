e5abbc0fb2f23d9b21e451d3a94c33de4d746b0c
xSuui <samuel2012rogerio@gmail.com>
2023-10-04 15:51:35 -0300

    update UI in classic mode

--- Assets/Scripts/Items/IitemManager.cs
+++ Assets/Scripts/Items/IitemManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Ebac.Core.Singleton;
 
 public class IitemManager : Singleton<IitemManager>
 {
     public int coins;
+    public TextMeshProUGUI uiTextCoins;
 
 
     private void Start()
@@ -16,10 +18,17 @@ public class IitemManager : Singleton<IitemManager>
     private void Reset()
     {
         coins = 0;
+        UpdateUI();
     }
 
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        uiTextCoins.text = coins.ToString();
     }
 }
